27d9541fc91e930cb6ce1e93e12241daeb572486
Circular-test <nmartinez@corpit-consulting.com>
2019-07-29 15:33:11 -0300

    New module

--- main.tf
+++ main.tf
@@ -1,1 +1,6 @@
-
+resource "aws_api_gateway_api_key" "gw-api-key" {
+  name        = "${var.name}"
+  enabled     = "${var.enabled}"
+  description = "${var.description}"
+  value       = "${var.value}"
+}
